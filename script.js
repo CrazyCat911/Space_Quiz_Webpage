0fd1b9adadc9da9f601b974c923957f576653d9c
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2023-05-12 13:11:02 +1000

    Fixed bug

--- script.js
+++ script.js
@@ -1,25 +1,166 @@
-var welcome = document.getElementById("welcome");
-var welcomeMessage = document.getElementById("welcomeMessage");
-var welcomeNext = document.getElementById("welcomeNext");
-var questionOne = document.getElementById("questionOne");
-var questionOneAnswer = document.getElementById("questionOneAnswer");
-var questionOneReturn = document.getElementById("questionOneReturn");
-var questionOneNext = document.getElementById("questionOneNext");
-var questionTwo = document.getElementById("questionTwo");
-var questionTwoAnswer = document.getElementById("questionTwoAnswer");
-var questionTwoReturn = document.getElementById("questionTwoReturn");
-var questionTwoNext = document.getElementById("questionTwoNext");
-var questionTwoPluto = document.getElementById("questionTwoPluto");
-var questionThree = document.getElementById("questionThree");
-var questionThreeAnswer = document.getElementById("questionThreeAnswer");
-var questionThreeReturn = document.getElementById("questionThreeReturn");
-var questionThreeNext = document.getElementById("questionThreeNext");
-var questionFour = document.getElementById("questionFour");
+const welcome = document.getElementById("welcome");
+const welcomeMessage = document.getElementById("welcomeMessage");
+const welcomeNext = document.getElementById("welcomeNext");
+const questionOne = document.getElementById("questionOne");
+const questionOneAnswer = document.getElementById("questionOneAnswer");
+const questionOneReturn = document.getElementById("questionOneReturn");
+const questionOneNext = document.getElementById("questionOneNext");
+const questionTwo = document.getElementById("questionTwo");
+const questionTwoAnswer = document.getElementById("questionTwoAnswer");
+const questionTwoReturn = document.getElementById("questionTwoReturn");
+const questionTwoNext = document.getElementById("questionTwoNext");
+const questionTwoPluto = document.getElementById("questionTwoPluto");
+const questionThree = document.getElementById("questionThree");
+const questionThreeAnswer = document.getElementById("questionThreeAnswer");
+const questionThreeReturn = document.getElementById("questionThreeReturn");
+const questionThreeNext = document.getElementById("questionThreeNext");
+const questionFour = document.getElementById("questionFour");
 const questionFourInput = document.querySelectorAll('input[name="questionFourInput"]')
-var questionFourInputOne = document.getElementById("questionFourInputOne");
-var questionFourInputTwo = document.getElementById("questionFourInputTwo");
-var questionFourAnswer = document.getElementById("questionFourAnswer");
-var questionFourButton = document.getElementById("questionFourButton");
+const questionFourInputOne = document.getElementById("questionFourInputOne");
+const questionFourInputTwo = document.getElementById("questionFourInputTwo");
+const questionFourButton = document.getElementById("questionFourButton");
+
+const urlParams = new URLSearchParams(window.location.search);
+
+if (urlParams.has("questionFourInput")) {
+    if (urlParams.get("questionFourInput") == "correct") {
+        welcomeMessage.textContent = "Well Done!";
+        welcomeNext.textContent = "Restart";
+
+        function clearText() {
+            questionOneReturn.textContent = '';
+            questionTwoReturn.textContent = '';
+            questionTwoPluto.classList.add('hidden');
+            questionThreeAnswer.textContent = "";
+        }
+        
+        function goToQuestionOne() {
+            welcome.classList.add("hidden");
+            questionOne.classList.remove("hidden");
+        }
+        
+        welcomeNext.onclick = goToQuestionOne;
+        
+        
+        questionOneAnswer.addEventListener("keyup", function(event) {
+            if (event.key === "Enter") {
+                if (questionOneAnswer.value == '0' || questionOneAnswer.value.toLowerCase() == 'zero') {
+                    questionOneReturn.textContent = 'Correct!';
+                    questionOneReturn.classList.add('correct');
+                    questionOneReturn.classList.remove('wrong');
+                    questionOneNext.classList.remove('hidden');
+                } else {
+                    questionOneReturn.textContent = 'Wrong!';
+                    questionOneReturn.classList.add('wrong');
+                    questionOneReturn.classList.remove('correct');
+                }
+            }
+        });
+        
+        questionOneAnswer.oninput = clearText;
+        
+        
+        function goToQuestionTwo() {
+            questionOne.classList.add('hidden');
+            questionTwo.classList.remove('hidden');
+        }
+        
+        questionOneNext.onclick = goToQuestionTwo;
+        
+        questionTwoAnswer.addEventListener("keyup", function(event) {
+            if (event.key === "Enter") {
+                if (questionTwoAnswer.value == '8' || questionTwoAnswer.value.toLowerCase() == 'eight') {
+                    questionTwoReturn.textContent = 'Correct!';
+                    questionTwoReturn.classList.add('correct');
+                    questionTwoReturn.classList.remove('wrong');
+                    questionTwoNext.classList.remove('hidden');
+                    questionTwoReturn.classList.remove('maybe');
+                } else if (questionTwoAnswer.value == '9' || questionTwoAnswer.value.toLowerCase() == 'nine') {
+                    questionTwoPluto.classList.remove('hidden');
+                    questionTwoPluto.classList.remove('correct');
+                    questionTwoPluto.classList.remove('wrong');
+                } else {
+                    questionTwoReturn.textContent = 'Wrong!';
+                    questionTwoReturn.classList.add('wrong');
+                    questionTwoReturn.classList.remove('correct');
+                    questionTwoReturn.classList.remove('maybe');
+                }
+            }
+        });
+        
+        questionTwoAnswer.oninput = clearText;
+        
+        function goToQuestionThree() {
+            questionTwo.classList.add('hidden');
+            questionThree.classList.remove('hidden');
+        }
+        
+        questionTwoNext.onclick = goToQuestionThree;
+        
+        questionThreeAnswer.addEventListener("keyup", function(event) {
+            if (event.key === "Enter") {
+                if (questionThreeAnswer.value.toLowerCase().includes('milky way')) {
+                    questionThreeReturn.textContent = 'Correct!';
+                    questionThreeReturn.classList.add('correct');
+                    questionThreeReturn.classList.remove('wrong');
+                    questionThreeNext.classList.remove('hidden');
+                } else {
+                    questionThreeReturn.textContent = 'Wrong!';
+                    questionThreeReturn.classList.add('wrong');
+                    questionThreeReturn.classList.remove('correct');
+                }
+            }
+        });
+        
+        function goToQuestionFour() {
+            questionThree.classList.add('hidden');
+            questionFour.classList.remove('hidden');
+        }
+        
+        questionThreeNext.onclick = goToQuestionFour;
+        
+        function questionFourCheck() {
+            for (const radioButton of questionFourInput) {
+                if (radioButton.checked) {
+                    if (radioButton.value = 'correct') {
+                        questionFourAnswer.textContent = "Correct!";
+                        break;
+                    } else {
+                        questionFourAnswer.textContent = "Wrong!";
+                    }
+                }
+            }
+        }
+        
+        questionFourButton.onclick = questionFourCheck;
+        
+        } else {
+            welcomeMessage.textContent = "You got it wrong!";
+            welcomeNext.textContent = "Retry";
+
+            function goToQuestionFour() {
+                welcome.classList.add("hidden");
+                questionFour.classList.remove("hidden");
+            }
+
+            welcomeNext.onclick = goToQuestionFour;
+
+            function questionFourCheck() {
+                for (const radioButton of questionFourInput) {
+                    if (radioButton.checked) {
+                        if (radioButton.value = 'correct') {
+                            questionFourAnswer.textContent = "Correct!";
+                            break;
+                        } else {
+                            questionFourAnswer.textContent = "Wrong!";
+                        }
+                    }
+                }
+            }
+            
+            questionFourButton.onclick = questionFourCheck;
+        }
+} else {
 
 
 
@@ -129,3 +270,5 @@ function questionFourCheck() {
 }
 
 questionFourButton.onclick = questionFourCheck;
+
+}
